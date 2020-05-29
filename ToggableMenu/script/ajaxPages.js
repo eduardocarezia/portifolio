9e19d69f2589d30f813f2eb41ab2e3204bc87257
Eduardo Carezia <eduardocarezia@gmail.com>
2020-05-29 08:31:04 -0300

    Add files via upload

---
+++ ToggableMenu/script/ajaxPages.js
@@ -0,0 +1,21 @@
+const about = `<h1>About</h1>
+<br>
+<p>
+  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti expedita nam harum nisi quasi tempore sint, illum
+  libero atque accusamus esse incidunt quibusdam voluptate doloremque non accusantium, quidem dolorum officiis.
+</p>`
+
+const contact = `<h1>Contact</h1>
+<br>
+<p>
+  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti expedita nam harum nisi quasi tempore sint, illum
+  libero atque accusamus esse incidunt quibusdam voluptate doloremque non accusantium, quidem dolorum officiis.
+</p>`
+
+const home = `<h1>Home</h1>
+<br>
+<p>
+  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti expedita nam harum nisi quasi tempore sint, illum
+  libero atque accusamus esse incidunt quibusdam voluptate doloremque non accusantium, quidem dolorum officiis.
+</p>`
+
